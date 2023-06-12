69cc573950012723622ff7509f0ee8693d342ad7
Ana <anavitoria.goes@hotmail.com>
2023-06-12 10:11:31 -0300

    diadosnamorados

---
+++ script.js
@@ -0,0 +1,22 @@
+let card = document.getElementById("card");
+
+// Exibir a mensagem caso seja selecionado o 'sim'
+document.querySelector("#yes").addEventListener("click", function () {
+  let question = document.getElementById("question");
+  let message = document.getElementById("answer");
+
+  card.style.backgroundImage =
+    "url('https://img1.picmix.com/output/stamp/normal/5/1/0/1/1171015_f458c.gif')";
+  question.style.display = "none";
+  message.style.display = "flex";
+});
+
+// Movimento aleatório do botão 'não'
+document.querySelector("#no").addEventListener("mouseover", function () {
+  let width = window.innerWidth - 50;
+  let height = window.innerHeight - 50;
+
+  this.style.position = "absolute";
+  this.style.top = Math.random() * width + "px";
+  this.style.left = Math.random() * height + "px";
+});
